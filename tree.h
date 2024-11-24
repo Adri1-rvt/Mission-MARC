1933e16153e89687ceff5b0f869a4014af2447fd
Adrien Rivet <94009526+Adri1-rvt@users.noreply.github.com>
2024-11-24 16:58:01 +0100

    Update tree.h

--- tree.h
+++ tree.h
@@ -1,5 +1,5 @@
 //
-// Created by adrie on 30/10/2024.
+// Created by adrie on 22/10/2024.
 //
 
 /**==========DIRECTIVES DU PREPROCESSEUR==========*/
@@ -8,36 +8,72 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "stack.h"
 #include "map.h"
 #include "moves.h"
 
-#define NB_possibilities 5
-#define NB_choices 3
+#define NB_possibilities 9
+#define NB_choices 5
+#define MAX 18730
 
 
 /**==========PROTOTYPES DES FONCTIONS==========*/
+// Structure des feuilles
 typedef struct s_node
 {
     int value;
     int depth;
+    t_move mouvement;
     t_soil soil_type;
+    struct s_node *parent;
     struct s_node **sons;
-    int nbSons; // Taille physique du tableau
+    int nbSons;
 } t_node;
 
-t_move* random_possibilities();
+// Structure des pointeurs sur une feuille
+typedef struct s_node t_node, *p_node;
 
-t_move* remove_possibility(t_move*, int, int);
+// Structure des arbres
+typedef struct s_tree
+{
+    p_node root;
+} t_tree;
 
-t_node *createNode(int, int, t_localisation, t_map);
 
-t_node *create_all_Node(int, int, t_move*, t_localisation, t_map);
+// Fonction pour supprimer un mouvement d'une liste de mouvements
+// Paramètre : entiers, pointeur sur mouvement
+// Retour : pointeur sur mouvement
+t_move* remove_move(t_move*, int, int);
 
-int search_min(t_node *);
+// Fonction pour créer un nouveau nœud
+// Paramètre : entiers, mouvements, localisation, map, pointeur sur feuille
+// Retour : pounteur sur feuille
+p_node createNode(int nb_sons, int depth, t_move mouvement, t_localisation loc, t_map map, p_node node);
 
-int path_min(t_node *,int *, int *);
+// Fonction pour créer tous les nœuds de l'arbre des mouvements possibles
+// Paramètre : entiers, mouvement, pointeur sur mouvement, lovalisation, map, pointeur sur feuille
+// Retour : pointeur sur feuille
+p_node create_all_Node(int nb_poss, int depth, t_move mouvement, t_move* possibilities, t_localisation robot, t_map map, p_node parent_node);
+
+// Fonction pour créer un arbre des mouvements possibles
+// Paramètre : entiers, mouvement, pointeur sur mouvement, localisation, map
+// Retour : arbre
+t_tree create_tree(int nb_poss, int depth, t_move mouvement, t_move* possibilities, t_localisation robot, t_map map);
+
+// Fonction d'appel de search_min_node
+// Paramètre : arbre
+// Retour : entier
+int search_min(t_tree);
+
+//Fonction pour chercher une feuille de valeur minimum
+// Paramètre : pointeurr sur feuille
+// Retour : entier
+int search_min_node(p_node node);
+
+//Fonction pour chercher le nombre de valeur minimum
+// Paramètre : pointeur sur feuille, entier
+// Retour : entier
+int nb_min(t_node *node, int min);
 
 
 /**==========DIRECTIVES DU PREPROCESSEUR==========*/
